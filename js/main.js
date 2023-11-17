5792cd07aea57e2513748a176a63720fd2268ac5
Soth-Yogoth <midnight746@yandex.ru>
2023-11-17 19:58:39 +0200

    Hue Triangle

--- js/main.js
+++ js/main.js
@@ -7,9 +7,12 @@ animate();
 function init()
 {
     container = document.getElementById('container');
+
     scene = new THREE.Scene();
+    displayTriangle();
+
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 4000); 
-    camera.position.set(5, 5, 5);
+    camera.position.set(0, 0, 10);
     camera.lookAt(new THREE.Vector3(0, 0, 0)); 
     
     renderer = new THREE.WebGLRenderer( { antialias: false } );
@@ -38,4 +41,30 @@ function animate()
 function render()
 {
     renderer.render(scene, camera);
+}
+
+function displayTriangle()
+{
+    var geometry = new THREE.Geometry();
+
+    geometry.vertices.push(new THREE.Vector3(-3.0, -3.0, 0.0));
+    geometry.vertices.push(new THREE.Vector3(3.0, -3.0, 0.0));
+    geometry.vertices.push(new THREE.Vector3(0.0, 3.0, 0.0));
+
+    geometry.faces.push(new THREE.Face3(0, 1, 2));
+
+    geometry.faces[0].vertexColors[0] = new THREE.Color(0xff0000);
+    geometry.faces[0].vertexColors[1] = new THREE.Color(0x00ff00);
+    geometry.faces[0].vertexColors[2] = new THREE.Color(0xff00ff);
+
+    var triangleMaterial = new THREE.MeshBasicMaterial({
+        vertexColors: THREE.VertexColors,
+        wireframe: false,
+        side: THREE.DoubleSide
+    });
+    
+    var triangleMesh = new THREE.Mesh(geometry, triangleMaterial);
+    triangleMesh.position.set(0.0, 0.0, 0.0);
+    
+    scene.add(triangleMesh);
 }
